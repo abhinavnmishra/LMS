f9080a5129768ab65be11fc738490cb44ce9ecf2
Abhinav Mishra <abhinavmishraise@gmail.com>
2022-06-11 19:21:54 +0530

    otp feature

--- controllers/twilio.js
+++ controllers/twilio.js
@@ -2,6 +2,7 @@ const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_AUTH;
 const number = process.env.NUMBER;
 const host = process.env.HOST;
+const fetch = require('node-fetch-commonjs');
 
 const client = require('twilio')(accountSid, authToken);
 const asyncHandler = require('../middleware/async');
@@ -19,8 +20,6 @@ exports.sendResourceUrl = asyncHandler(async (req, res, next) => {
     res.status(200).send('<iframe width="560" height="315" src="https://www.youtube.com/embed/JWN6qNGJQbI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 });
 
-
-
 exports.sendLoginUrl = asyncHandler(async (req, res, next) => {
     client.messages
         .create({
@@ -30,6 +29,34 @@ exports.sendLoginUrl = asyncHandler(async (req, res, next) => {
         })
         .then(message => console.log(message.sid))
         .done();
+
+    res.status(200).send('Success!');
+});
+
+
+exports.otp = asyncHandler(async (req, res, next) => {
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("to", "+917044025570");
+    encodedParams.append("p", "zJJDjqvan7BApoHq3kbnkK9y5lstodkfArgcrvuROaaY1kPjdJYhbNVJCaAMvbid");
+    encodedParams.append("text", "Your otp is 3456");
+
+    const url = 'https://sms77io.p.rapidapi.com/sms';
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': '4f4cce1780msh079ce874cfd1627p1b64dcjsn80984404a809',
+            'X-RapidAPI-Host': 'sms77io.p.rapidapi.com'
+        },
+        body: encodedParams
+    };
+
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error('error:' + err));
 
     res.status(200).send('Success!');
 });
--- routes/twilio.js
+++ routes/twilio.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {
-    redirect, sendLoginUrl, sendResourceUrl
+    redirect, sendLoginUrl, sendResourceUrl, otp
 } = require('../controllers/twilio');
 const router = express.Router();
 
 router.get('/login', sendLoginUrl);
 router.get('/resource', sendResourceUrl);
 router.get('/redirect', redirect);
+router.get('/otp', otp);
+
 module.exports = router;
